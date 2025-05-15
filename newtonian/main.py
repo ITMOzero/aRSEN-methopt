5d820dd464a01c3f1555ffa02b9baaff36c78ebc
A1sha <145219180+A1shaITMO@users.noreply.github.com>
2025-05-15 16:46:06 +0300

    Update main.py

--- newtonian/main.py
+++ newtonian/main.py
@@ -8,7 +8,6 @@ from quasi_newton_method import bfgs_modified
 from scipy_method import scipy_method
 from functions import select_function
 
-# TODO сделать доп задания 1 и 2
 # TODO gif for f3-5 functions 
 def wrap_count(func):
     class Wrapper:
@@ -43,7 +42,7 @@ if __name__ == '__main__':
     grad = wrap_count(grad_f)
     hess = wrap_count(hess_f)
 
-    params = {'alpha_0': 1.0, 'q': 0.9, 'c': 0.9, 'max_iter': 100}
+    params = {'alpha_0': 1.0, 'q': 0.5, 'c': 0.5, 'max_iter': 100}
 
     # Newton method
     f.reset(); grad.reset(); hess.reset()
@@ -60,10 +59,13 @@ if __name__ == '__main__':
     x_bfgs, it_bfgs, path_bfgs = scipy_method(f, grad, x_0=x_0, method='BFGS', options={'gtol': 1e-6, 'maxiter': 100})
     print_info("BFGS", x_bfgs, it_bfgs, f.n_calls, grad.n_calls, hess.n_calls)
 
-    # DFP TODO repair method
-    # f.reset(); grad.reset(); hess.reset()
-    # x_dfp, it_dfp, path_dfp = dfp_method(f, grad, x_0=x_0, eps=1e-6, max_iter=1000, params=params)
-    # print_info("DFP", x_dfp, it_dfp, f.n_calls, grad.n_calls, hess.n_calls)
+    # Our BFGS
+    f.reset(); grad.reset()
+    x_bfgs_mod_x0 = np.array([x_0])
+    x_bfgs_mod_result, path_bfgs_mod = bfgs_modified(f,grad,x_bfgs_mod_x0,max_iter=1000,tol=1e-6)
+    print_info("BFGS (modified)", x_bfgs_mod_result[0], len(path_bfgs_mod), f.n_calls + grad.n_calls)
+
+
 
     f.reset(); grad.reset(); hess.reset()
     x_bfgs_optuna_x0, it_bfgs_optuna_trials, path_bfgs_optuna = bfgs_optimize_with_optuna(f, grad, eps=1e-6, max_iter=1000)
@@ -83,16 +85,6 @@ if __name__ == '__main__':
     x_bfgs_optimized, it_bfgs_optimized, path_bfgs_optimized = scipy_optimize_with_optuna(f, grad, hess=None, method='BFGS', options={'gtol': 1e-6, 'maxiter': 100})
     print_info("Optimized BFGS", x_bfgs_optimized, it_bfgs_optimized, f.n_calls, grad.n_calls, hess.n_calls)
 
-    # DFP method optimized with optuna
-    # f.reset(); grad.reset(); hess.reset()
-    # x_dfp_optimized, it_dfp_optimized, path_dfp_optimized = dfp_optimize_with_optuna(f, grad, eps=1e-6, max_iter=1000, max_iter_for_backtracking=100)
-    # print_info("DFP", x_dfp_optimized, it_dfp_optimized, f.n_calls, grad.n_calls, hess.n_calls)
-
-    f.reset(); grad.reset()
-    x_bfgs_mod_x0 = np.array([x_0])
-    x_bfgs_mod_result, path_bfgs_mod = bfgs_modified(f,grad,x_bfgs_mod_x0,max_iter=1000,tol=1e-6)
-    print_info("BFGS (modified)", x_bfgs_mod_result[0], len(path_bfgs_mod), f.n_calls + grad.n_calls)
-
     # Визуализация всех
     paths = {
         'Newton': path_newton,
@@ -102,7 +94,6 @@ if __name__ == '__main__':
         'Optimized Newton': path_newton_optimized,
         'Optimized BFGS': path_bfgs_optimized,
         'Optimized Newton-CG': path_ncg_optimized,
-        # Optimized 'DFP': path_dfp_optimized
     }
     for k, v in paths.items():
         plot_function(func, {k: v})
